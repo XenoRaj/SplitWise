5d2eaa11a98185165f97e8e77f24c124a5f9be7e
XenoRaj <rohan22416@gmail.com>
2025-11-21 05:32:37 +0000

    using real mail

--- backend/backend_project/settings.py
+++ backend/backend_project/settings.py
@@ -190,7 +190,7 @@ LOGGING = {
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # For development - prints to console (disabled for real email testing):
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
